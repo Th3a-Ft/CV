7d7144ed45ec8eebf441b7aed03e2dc4b4fecaa2
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-06-19 11:22:14 +0200

    #1 - trad pages

--- pages/work.vue
+++ pages/work.vue
@@ -3,40 +3,40 @@
     <h1 class="text-2xl font-bold text-center">Portfolio</h1>
     <hr class="w-1/2 mx-auto border-gray-100">
 
-    <p class="text-center">Vous êtes ?</p>
+    <p class="text-center">{{ $t("portfolio.title") }}</p>
     <div class="flex flex-row flex-nowrap justify-evenly pt-4">
         <div class="flex flex-col align-center">
             <button @click="comm = !comm">
                 <img src="~/public/img/comm.png"
                     class="p-4 w-1/2 mx-auto border-solid border-black border-1 rounded-full">
             </button>
-            <p class="text-sm text-center">Un pro de la comm ?</p>
+            <p class="text-sm text-center">{{ $t("portfolio.comm.question") }}</p>
         </div>
         <div class="flex flex-col align-center">
             <button @click="dev = !dev">
                 <img src="~/public/img/dev.png"
                     class="p-4 w-1/2 mx-auto border-solid border-black border-1 rounded-full">
             </button>
-            <p class="text-sm text-center">Un pro du dev ?</p>
+            <p class="text-sm text-center">{{ $t("portfolio.dev.question") }}</p>
         </div>
         <div class="flex flex-col align-center">
             <button @click="curious = !curious">
                 <img src="~/public/img/all.png"
                     class="p-4 w-1/2 mx-auto border-solid border-black border-1 rounded-full">
             </button>
-            <p class="text-sm text-center">Juste curieux</p>
+            <p class="text-sm text-center">{{ $t("portfolio.curious.question") }}</p>
         </div>
     </div>
     <hr class="w-1/2 mx-auto border-gray-150">
 
     <div v-if="comm">
-        <h1 class="font-bold text-center">Portfolio Comm</h1>
+        <h1 class="font-bold text-center">{{ $t("portfolio.comm.title") }}</h1>
     </div>
     <div v-if="dev">
-        <h1 class="font-bold text-center">Portfolio Dev</h1>
+        <h1 class="font-bold text-center">{{ $t("portfolio.dev.title") }}</h1>
     </div>
     <div v-if="curious">
-        <h1 class="font-bold text-center">Curieux</h1>
+        <h1 class="font-bold text-center">{{ $t("portfolio.curious.title") }}</h1>
     </div>
 </template>
 
