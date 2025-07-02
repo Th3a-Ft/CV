3c3083331948549afc56df65836d52f4d170c532
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-07-02 13:03:48 +0200

    feat: add CardSkills component and integrate skills localization in index page

---
+++ components/CardSkills.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="border-solid border-gray-100 border-1 rounded-lg shadow-2xl m-8 pb-8">
+        <h2 class="m-4 md:mt-8 font-bold text-center">{{ title }}</h2>
+        <div class="flex flex-col flex-nowrap justify-evenly px-8 md:mx-8">
+            <p v-for="skill in skills"
+                class="border-solid text-white bg-[#800101] border-1 rounded-full px-6 py-2 m-2 text-center">{{ skill }}
+            </p>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+const props = defineProps({
+    title: String,
+    skills: Array,
+
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,37 @@
 <template>
-    <h1 class="text-center font-extrabold text-2xl">De la communication au développement</h1>
+    <h1 class="mt-8 text-center font-extrabold text-2xl">{{ $t('title.index') }}</h1>
+    <p class="text-center m-8"> {{ $t('general.descriptionIndex') }} </p>
+    <h2 class="text-center font-bold text-xl"></h2>
+    <div class="flex flex-row flex-wrap lg:flex-wrap lg:justify-evenly">
+        <CardSkills
+            :skills="[$t('skills.dev.languages'), $t('skills.dev.algorithm'), $t('skills.dev.agile'), 'PHP (Laravel)', 'Git']"
+            :title="$t('skills.dev.titleCard')" class="w-full lg:w-1/4" />
+
+        <CardSkills :skills="['Wordpress', 'Webself']" :title="$t('skills.nocode.titleCard')" class="w-full lg:w-1/4" />
+
+        <CardSkills :skills="['Canva', 'Photoshop', 'Illustrator', 'Inkscape', 'Capcut']"
+            :title="$t('skills.software.titleCard')" class="w-full lg:w-1/4" />
+
+
+        <CardSkills :skills="['Facebook', 'Instagram', 'Linkedin']" :title="$t('skills.socialMedia.titleCard')"
+            class="w-full lg:w-1/4" />
+
+
+        <CardSkills :skills="['Brevo', 'Mailchimp']" :title="$t('skills.newsletter.titleCard')"
+            class="w-full lg:w-1/4" />
+
+        <CardSkills :skills="[$t('skills.languages.english')]" :title="$t('skills.languages.titleCard')"
+            class="w-full lg:w-1/4" />
+
+
+
+    </div>
+
+
+
+
+
+
 
 
 
